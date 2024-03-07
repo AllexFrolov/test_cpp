f477e7d62a4110291d49ff691bec5bf50cb1da3d
AllexFrolov <mosbka.37@gmail.com>
2024-03-07 15:41:53 +0300

    add Cmake

---
+++ tests/test.cpp
@@ -0,0 +1,13 @@
+// #include "src/Card.hpp"
+// #include "src/Deck.hpp"
+// #include "src/utils.hpp"
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
